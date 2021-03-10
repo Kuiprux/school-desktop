fd6046aa5873c29bc0f40cce999e91a3fec91c4a
Kuiprux <kuiprux@gmail.com>
2021-03-11 08:17:58 +0900

    Removed Calendar mode entry

--- start.js
+++ start.js
@@ -56,7 +56,7 @@ function createWindow () {
   //tray.setContextMenu(contextMenu);
 
   mainTrayMenu = Menu.buildFromTemplate([
-    { label: 'Calendar Mode', click: swapMode },
+    //{ label: 'Calendar Mode', click: swapMode },
     { label: 'Quit', click: ()=>{win.close();} }
   ]);
   calTrayMenu = Menu.buildFromTemplate([
